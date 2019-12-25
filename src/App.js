89431890776e2e5114b9ed88e1c68daf134887aa
ITch8 <lichuang516@126.com>
2019-12-25 14:20:43 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {GlobalStyled} from './style.js'
+function App() {
+  return (
+    <div>
+		<GlobalStyled/>
+     Hello lc
+    </div>
+  );
+}
+
+export default App;
